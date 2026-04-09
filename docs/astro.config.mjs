73828773be6914d931e913ebea959ee5dccaea39
Piotr Piech <piotr@piech.dev>
2026-04-10 01:24:03 +0200

    refactor (#5)
    Squashed after the fact from GitHub merge commit: Merge pull request #5 from Tenemo/dev.
    
    * 59f1cee refactor

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -20,7 +20,7 @@ export default defineConfig({
             customCss: ['./src/styles/custom.css'],
             editLink: {
                 baseUrl:
-                    'https://github.com/Tenemo/sealed-lattice/edit/master/docs/src/content/docs/',
+                    'https://github.com/Tenemo/sealed-lattice/edit/master/docs/',
             },
             sidebar: [
                 {
